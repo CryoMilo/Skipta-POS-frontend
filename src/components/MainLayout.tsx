09f452c3709bf89a9f1c0fc6cc22b5b3d942b70d
CryoMilo <minkojee@gmail.com>
2024-09-24 08:53:31 +0700

    minor card fixes

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 
 const MainLayout = (props: { children: ReactNode }) => {
 	return (
-		<div className="flex h-screen">
+		<div className="flex h-full min-h-screen">
 			<Sidebar />
 			<div className="m-10 w-full">{props.children}</div>
 		</div>
