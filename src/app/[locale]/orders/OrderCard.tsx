b17b51ad240eae90c012d6d4e9be8b06d65ecca8
Damian Aung <damianaung@Damians-MacBook-Air.local>
2024-10-25 18:19:38 +0700

    added order card ui

--- src/app/[locale]/orders/OrderCard.tsx
+++ src/app/[locale]/orders/OrderCard.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
-import { Edit, Info, Minus, Plus } from "lucide-react";
-import Link from "next/link";
+import { Minus, Plus } from "lucide-react";
 import { getImageSrc } from "@/utils/getImgSrc";
 import { Order } from "@/types/orders";
 
@@ -18,7 +11,7 @@ interface OrderCardProps {
 }
 
 const OrderCard: React.FC<OrderCardProps> = ({ order }) => {
-	const { _id, image, productName, description, vegan } = order;
+	const { productName } = order;
 
 	// const addOrder = async () => {
 	// 	try {
@@ -39,11 +32,11 @@ const OrderCard: React.FC<OrderCardProps> = ({ order }) => {
 	// };
 
 	return (
-		<Card key={_id} className="group w-56 rounded-xl bg-primary">
+		<Card key={productName} className="group w-56 rounded-xl bg-primary">
 			<CardContent className="relative flex flex-col p-1">
 				<div className="relative h-48 w-full">
 					<Image
-						src={getImageSrc(image)}
+						src={getImageSrc("")}
 						alt={productName}
 						fill
 						className="w-60 rounded-xl object-cover p-1"
@@ -54,9 +47,6 @@ const OrderCard: React.FC<OrderCardProps> = ({ order }) => {
 					<CardTitle className="truncate text-left text-secondary">
 						{productName}
 					</CardTitle>
-					<CardDescription className="line-clamp-2 h-10 text-secondary">
-						{description}
-					</CardDescription>
 				</CardHeader>
 
 				<section className="flex w-full items-center justify-around py-3 text-secondary">
